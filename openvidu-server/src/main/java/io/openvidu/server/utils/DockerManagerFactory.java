5dce0c199c5c8a439afabee001ce60b519172548
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2021-01-18 17:36:50 +0100

    Added log messages to DMFactory

--- openvidu-server/src/main/java/io/openvidu/server/utils/DockerManagerFactory.java
+++ openvidu-server/src/main/java/io/openvidu/server/utils/DockerManagerFactory.java
@@ -1,18 +1,27 @@
 package io.openvidu.server.utils;
 
 import io.openvidu.server.config.OpenviduConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DockerManagerFactory {
+    private static final Logger log = LoggerFactory.getLogger(DockerManagerFactory.class);
+
     public static DockerManager create(OpenviduConfig openviduConfig) {
         final String openViduRecordingDockerHelperUrl = openviduConfig.getOpenViduRecordingDockerHelperUrl();
-        if (openViduRecordingDockerHelperUrl == null)
+        if (openViduRecordingDockerHelperUrl == null) {
+            log.debug("Using local Docker Manager for recording");
             return new LocalDockerManager(false);
+        }
+        log.debug("Using externalized Docker Manager (URL: {}) for recording", openViduRecordingDockerHelperUrl);
         return new ExternalizedDockerManager(false, openViduRecordingDockerHelperUrl);
     }
 
     public static DockerManager createAndInitialize(OpenviduConfig openviduConfig) {
         DockerManager dockerManager = create(openviduConfig);
+        log.debug("Initializing recording's Docker Manager");
         dockerManager.init();
+        log.debug("Initialized recording's Docker Manager");
         return dockerManager;
     }
 }
