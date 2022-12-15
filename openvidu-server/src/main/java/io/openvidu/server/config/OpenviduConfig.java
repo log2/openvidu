68bac512e7cd4c55109ef6d981d2153cbf6543a7
Antonio Serricchio <a.serricchio@eco-mind.eu>
2022-12-15 11:47:37 +0100

    Added: dynamic turn/stun config

--- openvidu-server/src/main/java/io/openvidu/server/config/OpenviduConfig.java
+++ openvidu-server/src/main/java/io/openvidu/server/config/OpenviduConfig.java
@@ -177,6 +177,10 @@ public class OpenviduConfig {
 
 	private boolean openviduWebhookEnabled;
 
+	private String coturnHostSubstFrom;
+
+	private String coturnHostSubstTo;
+
 	private String openviduWebhookEndpoint;
 
 	private List<Header> webhookHeadersList;
@@ -375,6 +379,14 @@ public class OpenviduConfig {
 
 	public String getOpenViduWebhookEndpoint() {
 		return this.openviduWebhookEndpoint;
+	}
+
+	public String getCoturnHostSubstFrom() {
+		return coturnHostSubstFrom;
+	}
+
+	public String getCoturnHostSubstTo() {
+		return coturnHostSubstTo;
 	}
 
 	public List<Header> getOpenViduWebhookHeaders() {
@@ -635,6 +647,9 @@ public class OpenviduConfig {
 
 		openviduSessionsGarbageInterval = asNonNegativeInteger("OPENVIDU_SESSIONS_GARBAGE_INTERVAL");
 		openviduSessionsGarbageThreshold = asNonNegativeInteger("OPENVIDU_SESSIONS_GARBAGE_THRESHOLD");
+
+		coturnHostSubstFrom = asOptionalString("COTURN_HOST_SUBST_FROM");
+		coturnHostSubstTo = asOptionalString("COTURN_HOST_SUBST_TO");
 
 		openviduForcedCodec = asEnumValue("OPENVIDU_STREAMS_FORCED_VIDEO_CODEC", VideoCodec.class);
 		openviduAllowTranscoding = asBoolean("OPENVIDU_STREAMS_ALLOW_TRANSCODING");
